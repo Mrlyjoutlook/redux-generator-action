fb90d550a8924b1a9c3145c7005651a9a392d935
连远杰 <lianyuanjie@lianyuanjiedeMacBook-Pro.local>
2017-03-21 21:27:09 +0800

    继续完善单元测试

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,43 +1,23 @@
-const generatorAction = require('../lib/'),
-      expect = require('chai').expect;
+const generatorAction = require('../lib/index'),
+      chai = require('chai'),
+      spies = require('chai-spies');
 
-const doDispatch = () => {};
-  const doGetState = () => {};
+      chai.use(spies);
+const expect = chai.expect;
 
-describe('Test array', function () {
+describe('Test 简单数组', function () {
     it('正常按顺序执行', function () {
+
+        const doDispatch = () => {};
+        const doGetState = () => {};
+        const nextMiddleware = generatorAction({dispatch: doDispatch, getState: doGetState});
+        
         const action = [
             {type:'AA'},
             {type:'BB'},
             {type:'CC'}
         ];
-        var spy = chai.spy(next);
-        sequenceAction({})(spy)(action);
 
-        expect(spy).to.have.been.called.with(action);
-    });
-});
-
-describe('Test whether the array contains an array', function () {
-    it('adaw', function () {
-        expect(generatorAction(1, 1)).to.be.equal(2);
-    });
-});
-
-describe('Test whether the array contains an object(type is all)', function () {
-    it('adaw', function () {
-        expect(generatorAction(1, 1)).to.be.equal(2);
-    });
-});
-
-describe('Test whether the array contains an object(type is race)', function () {
-    it('adaw', function () {
-        expect(generatorAction(1, 1)).to.be.equal(2);
-    });
-});
-
-describe('Test whether the array contains an array and an object(type is all or race)', function () {
-    it('adaw', function () {
-        expect(generatorAction(1, 1)).to.be.equal(2);
+        chai.assert.isObject(nextMiddleware);
     });
 });
